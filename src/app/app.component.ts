0e7a0c01fa0541e89fe18b1c88917e06fd391a29
SudhakarDhayalan <sudhakar.dhayalan@riskcovry.com>
2021-05-08 13:06:36 +0530

    animation completed

---
+++ src/app/app.component.ts
@@ -0,0 +1,104 @@
+import { state, style, trigger, transition, animate, group } from '@angular/animations';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        backgroundColor: 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'purple',
+        transform: 'translateX(100px)'
+      })),
+      transition('normal <=> highlighted', animate(600))
+    ]),
+
+    trigger('wildState', [
+      state('normal', style({
+        backgroundColor: 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'purple',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        backgroundColor: 'green',
+        transform: 'translateX(0) scale(0.5)'
+      })),
+      transition('normal => highlighted', animate(1000)),
+      transition('highlighted => normal', animate(600)),
+      transition('shrunken <=> *', [
+        style({
+          backgroundColor: 'orange'
+        }),
+        animate(1000, style({
+          borderRadius: '50px'
+        })),
+        animate(500)
+      ])
+    ]),
+
+    // while adding item
+    trigger('list1', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(600)
+      ]),
+
+      // while removing item
+      // transition('* => void', [
+      //   animate(600, style({
+      //     opacity: 0,
+      //     transform: 'translateX(100px)'
+      //   }))
+      // ]),
+
+      // while removing item, grouping item - will apply animations SYNCHRONOUSLY
+      transition('* => void', [
+        group([
+          animate(300, style({
+            color: 'red'
+          })),
+          animate(600, style({
+            opacity: 0,
+            transform: 'translateX(100px)'
+          }))
+        ])
+      ])
+    ]),
+  ]
+})
+export class AppComponent {
+  state = 'normal';
+  wildState = 'normal';
+  list = ['Milk', 'Sugar', 'Bread'];
+
+  onAnimate() {
+    this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState == 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+  }
+
+  onShrink() {
+    this.wildState = 'shrunken';
+  }
+
+  onAdd(item) {
+    this.list.push(item);
+  }
+
+  onDelete(item) {
+    this.list.splice(this.list.indexOf(item), 1)
+  }
+}
